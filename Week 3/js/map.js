0f63fd48404c31019b492d4378a67d2852a42de2
LynnT2 <59623146+LynnT2@users.noreply.github.com>
2021-04-18 18:55:18 -0700

    woo

--- Week 3/js/map.js
+++ Week 3/js/map.js
@@ -3,63 +3,77 @@ let data = [
 'title':'1. Janss Steps',
 'lat': 34.072038,
 'lon': -118.443152,
-'des': 'Great for naps'
+'des': 'Another iconic part of UCLA are its Janss Steps, and is a popular choice for graduates to show off their sashes.',
+'url': 'https://user-images.githubusercontent.com/59623146/115165942-69acb780-a065-11eb-873d-1117a5a8332f.jpg'
 },
 {
 'title':'2. Royce Hall',
 'lat': 34.0729060754462,
 'lon':  -118.44215374487563,
-'des': 'A UCLA classic, great during sunsets'
+'des': "A UCLA classic. You can't graduate from UCLA without getting a shot under the arches of Royce Hall! The lighting is especially great during sunsets.",
+'url': 'https://user-images.githubusercontent.com/59623146/115165675-e2ab0f80-a063-11eb-9938-ae775580fc5b.jpg',
 },
 {
 'title':'3. BruinWalk',
 'lat': 34.070964,
 'lon': -118.444235,
-'des': 'Best dog/people watch spot'
+'des': 'BruinWalk has its pros and cons, but at least the scenery is perfect for framing those action shots on campus!',
+'url': 'https://user-images.githubusercontent.com/59623146/115165847-c360b200-a064-11eb-97d8-ce35e32328af.jpg',
 },
 {
 'title':'4. John Wooden Center',
 'lat': 34.071279744884514, 
 'lon': -118.44569962517272,
-'des': 'Where to practice martial arts at UCLA'
+'des': 'For the students who lived for the gains, why not use your favorite gym on campus as a photoshoot location? Flex your degree and those muscles!',
+'url': 'https://user-images.githubusercontent.com/59623146/115165463-da060980-a062-11eb-8944-5c845213247e.jpg',
 },
 {
-'title':'5. Rendevous',
-'lat': 34.07274610988398, 
-'lon': -118.45172923104543,
-'des': 'Take-out food galore!'
+'title':'5. Kerckhoff Patio',
+'lat': 34.07042259617002, 
+'lon': -118.44313217139769,
+'des': 'Kerckhoff Patio is an uncommon spot for grad photos, but the towers of Kerkchoff Hall, the natural table and chair props, and the nostalgia of cramming in-between classes makes it a special place to commemorate your years at UCLA.',
+'url': 'https://user-images.githubusercontent.com/59623146/115161650-c56e4500-a053-11eb-80cf-4219b77e9507.jpg',
 }
 ]	
 
-var map = L.map('map').setView([34.073127516258694, -118.44690850943654,], 16);
+var map = L.map('map').setView([34.07122126361896, -118.44350912795157,], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+// customized marker
+var pawIcon = L.icon({
+    iconUrl: 'https://user-images.githubusercontent.com/59623146/115158510-55f05980-a043-11eb-96b0-00f8deb731a3.png',
+    iconSize:     [38, 38], // size of the icon
+    iconAnchor:   [23.5, 47], // point of the icon which will correspond to marker's location
+    popupAnchor:  [200, 100] // point from which the popup should open relative to the iconAnchor
+});
 
 // before looping the data, create an empty FeatureGroup
 let myMarkers = L.featureGroup();
 
 // function to fly to a location by a given id number
 function flyByIndex(index){
-	map.flyTo([data[index].lat,data[index].lon],25)
-
-	// open the popup
-	myMarkers.getLayers()[index].openPopup()
+	map.flyTo([data[index].lat,data[index].lon], 18);
 }
 
 // loop through data
 data.forEach(function(item,index){
 	// add marker to map
-	let marker = L.marker([item.lat,item.lon]).addTo(map)
-		.bindPopup(item.title)
+	let marker = L.marker([item.lat,item.lon],{icon: pawIcon}).addTo(map)
+		.bindPopup("<h2>" + item.title + "</h2>"+ "<center><img src ='"+ item.url + "'width=100%'/></center>" +
+		item.des)
 
     // add marker to featuregroup
 	myMarkers.addLayer(marker)
 
 	// add data to sidebar with onclick event
-	$('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${index})">${item.title}</div>`)
-})
+	$('.sidebar').append(`<div class = "sidebar-item" onclick="flyToIndex(${index});ShowAndHide(${index})">${item.title}</div>`)
+	//add paragraph div in side-bar
+	$('.sidebar').append(`<div id = "${index}" style="display: none">${item.des}</div>`)
+	
+});
 
 // after loop, add the FeatureGroup to map
 myMarkers.addTo(map)
@@ -71,4 +85,19 @@ let layers = {
 
 // add layer control box
 L.control.layers(null,layers).addTo(map)
+
+// make the map zoom to the extent of markers
+map.fitBounds(myMarkers.getBounds());
+
+//add button on map for default view
+L.easyButton('fa-globe', function(btn,map){
+	map.fitBounds(myMarkers.getBounds());
+}, 'default view').addTo(map);
+
+function flyToIndex(index){
+	map.flyTo([data[index].lat, data[index].lon], 18)
+	myMarkers.getLayers()[index].openPopup() // for having popup open up automatically when flying 
+};
+
+
 
